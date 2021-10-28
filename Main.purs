bbcd706d3e9eaba92a1a3cf5f92749331d3c16a7
Aaditya <46195453+aadijay3477@users.noreply.github.com>
2021-10-28 19:35:24 +0530

    Add files via upload
    learning purescript

---
+++ Main.purs
@@ -0,0 +1,16 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (logShow)
+import Math (pi, sqrt)
+
+main :: Effect Unit
+main = logShow (diagonal 3.0 4.0)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt(w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = 4.0*pi*r*r
